7bdd916932b74e19177d1257bb7e51de91692b0c
DanielMcNeil <daniel.mcneil@outlook.com>
2016-08-31 16:41:30 -0400

    Register for Activities section
    Display total price for activities checked.  If there are 2 activities at the same time, grey out one when the other is checked.

--- js/app.js
+++ js/app.js
@@ -28,3 +28,51 @@ $('#design').change(function(){
     $('#color').html('');
   }
 });
+
+// initialize total price for activities and add event handler to activities check boxes
+var total = 0;
+$('.activities input').click(function(){
+  // Display total price for activities checked
+  if ($(this).attr('name') === 'all' && $(this).prop('checked')) {
+    total += 200;
+  } else if ($(this).attr('name') === 'all' && !$(this).prop('checked')) {
+    total -= 200;
+  } else if ($(this).prop('checked')) {
+    total += 100;
+  } else if (!$(this).prop('checked')) {
+    total -= 100;
+  }
+
+  var displayTotal = '$' + total;
+  $('#total').attr('value',displayTotal);
+
+  // disable activity choice if user chooses another one that is scheduled at the same time
+    // JavaScript Frameworks and Express
+  if ($(this).attr('name') === 'js-frameworks' && $(this).prop('checked')) {
+    $('input[name=express]').attr('disabled',true);
+    $('input[name=express]').parent().addClass('greyed-out');
+  } else if ($(this).attr('name') === 'js-frameworks' && !$(this).prop('checked')) {
+    $('input[name=express]').attr('disabled',false);
+    $('input[name=express]').parent().removeClass('greyed-out');
+  } else if ($(this).attr('name') === 'express' && $(this).prop('checked')) {
+    $('input[name=js-frameworks]').attr('disabled',true);
+    $('input[name=js-frameworks]').parent().addClass('greyed-out');
+  } else if ($(this).attr('name') === 'express' && !$(this).prop('checked')) {
+    $('input[name=js-frameworks]').attr('disabled',false);
+    $('input[name=js-frameworks]').parent().removeClass('greyed-out');
+  }
+    // JavaScript Libries and Node.js
+   if ($(this).attr('name') === 'js-libs' && $(this).prop('checked')) {
+    $('input[name=node]').attr('disabled',true);
+    $('input[name=node]').parent().addClass('greyed-out');
+  } else if ($(this).attr('name') === 'js-libs' && !$(this).prop('checked')) {
+    $('input[name=node]').attr('disabled',false);
+    $('input[name=node]').parent().removeClass('greyed-out');
+  } else if ($(this).attr('name') === 'node' && $(this).prop('checked')) {
+    $('input[name=js-libs]').attr('disabled',true);
+    $('input[name=js-libs]').parent().addClass('greyed-out');
+  } else if ($(this).attr('name') === 'node' && !$(this).prop('checked')) {
+    $('input[name=js-libs]').attr('disabled',false);
+    $('input[name=js-libs]').parent().removeClass('greyed-out');
+  }
+});
